9bb49a84f592714fd207ff19387ddeca907e9434
AtifKhan9423 <atif.khan@ontariotechu.net>
2024-11-24 20:20:05 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+
+const assignmentRoutes = require('./routes/assignments');
+app.use('/assignments', assignmentRoutes);
+
+
+app.get('/', (req, res) => {
+  res.render('pages/home');
+});
+
+// Start the Server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
